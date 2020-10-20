5b9a62ae06a60545a40c82bf5d33426cf5fbf1c1
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-10-20 11:23:14 +0200

    AZ-248 Revert 0.13 upgrade. Need to be in separate MR

--- versions.tf
+++ versions.tf
@@ -1,9 +1,6 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2"
-    }
+    azurerm = ">= 2.0"
   }
 }
