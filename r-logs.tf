310aff26232c04c079100e97241403f7035bb1a3
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-08-25 17:41:28 +0200

    AZ-532: Revamp README with latest `terraform-docs` tool

--- r-logs.tf
+++ r-logs.tf
@@ -1,6 +1,6 @@
 module "diagnostics" {
   source  = "claranet/diagnostic-settings/azurerm"
-  version = "4.0.1"
+  version = "4.0.2"
 
   resource_id           = azurerm_key_vault.keyvault.id
   logs_destinations_ids = var.logs_destinations_ids
