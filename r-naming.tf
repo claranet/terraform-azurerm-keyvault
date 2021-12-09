420000f9483fec3fdf41a9b029fff32f3f1d9ffc
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-12-09 15:59:03 +0100

    AZ-515: Swap location & env order in resource naming

--- r-naming.tf
+++ r-naming.tf
@@ -2,7 +2,7 @@ resource "azurecaf_name" "keyvault" {
   name          = var.stack
   resource_type = "azurerm_key_vault"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, var.use_caf_naming ? "" : "kv"])
+  suffixes      = compact([var.client_name, var.environment, var.location_short, local.name_suffix, var.use_caf_naming ? "" : "kv"])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
