af4c4b105900612e0f86f57ce636550a02998593
Jérémy MARMOL <jeremy.marmol@claranet.com>
2024-10-04 15:41:23 +0200

    docs(AZ-1088): fix `logs_destinations_ids` variable description

--- variables-logs.tf
+++ variables-logs.tf
@@ -5,7 +5,7 @@ variable "logs_destinations_ids" {
   description = <<EOD
 List of destination resources IDs for logs diagnostic destination.
 Can be `Storage Account`, `Log Analytics Workspace` and `Event Hub`. No more than one of each can be set.
-If you want to specify an Azure EventHub to send logs and metrics to, you need to provide a formated string with both the EventHub Namespace authorization send ID and the EventHub name (name of the queue to use in the Namespace) separated by the `|` character.
+If you want to use Azure EventHub as a destination, you must provide a formatted string containing both the EventHub Namespace authorization send ID and the EventHub name (name of the queue to use in the Namespace) separated by the <code>&#124;</code> character.
 EOD
 }
 
